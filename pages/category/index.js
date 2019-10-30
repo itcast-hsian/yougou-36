28fcd97ea3ecea229f58b618abed5fa3a21517c8
itcast-hsian <541510140@qq.com>
2019-10-30 17:14:35 +0800

    分类页数据渲染

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request.js"
+
 // pages/category/index.js
 Page({
 
@@ -7,13 +9,24 @@ Page({
   data: {
     // 当前选中的索引
     current: 0,
+    // 分类列表
+    list: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 请求分类页数据
+    request({
+      url: "/api/public/v1/categories"
+    }).then(res => {
+      // 保存数据到data
+      const {message} = res.data;
+      this.setData({
+        list: message
+      })
+    })
   },
 
   // 点击左侧的菜单时候触发
