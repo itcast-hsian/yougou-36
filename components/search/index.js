b1254cbd811873596b66f7c8124981a8a56a76a4
itcast-hsian <541510140@qq.com>
2019-10-30 15:58:02 +0800

    新建search组件

---
+++ components/search/index.js
@@ -0,0 +1,31 @@
+// components/search/index.js
+Component({
+  /**
+   * 组件的属性列表，类似于vue中props
+   */
+  // vue中props
+  // props: {
+  //   data: {
+  //     type: Object,
+  //     default: {}
+  //   }
+  // },
+
+  properties: {
+    
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
