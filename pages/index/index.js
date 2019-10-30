fa9b8dd9dc2ea97327d29e35c24af737fafef497
itcast-hsian <541510140@qq.com>
2019-10-30 14:41:17 +0800

    请求菜单列表数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,7 @@ import request from "../../utils/request.js"
 Page({
 
   data: {
+    // 轮播图数据
     banners: [],
 
     menus: [
@@ -15,7 +16,7 @@ Page({
   
   onLoad(){
 
-    // 测试
+    // 请求轮播图数据
     request({
       url: "/api/public/v1/home/swiperdata"
     }).then(res => {
@@ -24,7 +25,20 @@ Page({
       this.setData({
         banners: message
       })
+    });
+
+    // 请求菜单数据
+    request({
+      url: "/api/public/v1/home/catitems"
+    }).then(res => {
+      const  {message} = res.data;
+
+      // 赋值给data中menus
+      this.setData({
+        menus: message
+      })
     })
+
   }
 
 })
