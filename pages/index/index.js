5a90b4c2d0931679df11a3ea3bbad87b06d72c47
itcast-hsian <541510140@qq.com>
2019-10-30 11:28:17 +0800

    首页轮播图数据

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,14 @@
 import request from "../../utils/request.js"
 
 Page({
+
+  data: {
+    banners: [
+      "https://cdn.pinduoduo.com/home/static/img/subject/girlclothes.jpg",
+      "https://cdn.pinduoduo.com/home/static/img/subject/boyshirt.jpg",
+      "https://cdn.pinduoduo.com/home/static/img/subject/medical.jpg"
+    ]
+  },
   
   onLoad(){
 
