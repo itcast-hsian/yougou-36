755147e94815b898433647d11292a326b7d134f4
itcast-hsian <541510140@qq.com>
2019-10-30 11:37:10 +0800

    菜单列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,13 @@ Page({
       "https://cdn.pinduoduo.com/home/static/img/subject/girlclothes.jpg",
       "https://cdn.pinduoduo.com/home/static/img/subject/boyshirt.jpg",
       "https://cdn.pinduoduo.com/home/static/img/subject/medical.jpg"
+    ],
+
+    menus: [
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
     ]
   },
   
