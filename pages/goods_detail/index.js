cc1265473d06be5df00ed291316b8caeec030c06
itcast-hsian <541510140@qq.com>
2019-10-31 17:08:27 +0800

    渲染详情页数据

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,3 +1,5 @@
+import request from "../../utils/request.js"
+
 // pages/goods_detail/index.js
 Page({
 
@@ -5,62 +7,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    detail: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (options) { 
+    // 获取id
+    const { goods_id } = options;
 
-  },
+    // 请求商品详情
+    request({
+      url: "/api/public/v1/goods/detail",
+      data: {
+        goods_id
+      }
+    }).then(res => {
+      const {message} = res.data;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      // detail是商品详情
+      this.setData({
+        detail: message
+      })
+    })
   }
 })
