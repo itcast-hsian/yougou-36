a37b736c38f63b8d5289b0963669aa14583586e9
itcast-hsian <541510140@qq.com>
2019-11-03 10:40:38 +0800

    把商品加入到本地的购物车

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -31,5 +31,31 @@ Page({
         detail: message
       })
     })
+  },
+
+  // 添加到本地的购物车
+  handleAddCart(){
+    // 从本地获取购物车列表
+    const goods = wx.getStorageSync("goods") || {};
+    const { goods_id, goods_name, goods_small_logo, goods_price} = this.data.detail;
+
+    // 判断商品是否已经在购物车中
+    
+
+    // 前面四个属性是商品详情提供的（需要在购物车页面中渲染）
+    // number和selected属性是自己定义给购物车页面使用的
+    // 使用对象的方式存储是方便快速查找属性，如果使用的数组，需要循环浪费性能
+    goods[goods_id] = {
+      goods_id,
+      goods_name,
+      goods_small_logo,
+      goods_price,
+
+      number: 1,
+      selected: true
+    }
+
+    // 保存到本地
+    wx.setStorageSync("goods", goods)
   }
 })
