93df9c85c4d81a080120e0336ef5b3d43ed56242
itcast-hsian <541510140@qq.com>
2019-11-03 09:32:19 +0800

    03-获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 收货地址
+    address: {},
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取收货地址
+  handleAddress(){
+    wx.chooseAddress({
+      // 成功的方法
+      success: (res) => {
+        // 设置收货地址
+        this.setData({
+          address: {
+            userName: res.userName,
+            telNumber: res.telNumber,
+            detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+      }
+    })
   }
 })
