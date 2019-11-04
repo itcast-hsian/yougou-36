fd5485d6a52307db1cbb72dd71870530a2b419a5
itcast-hsian <541510140@qq.com>
2019-11-04 14:45:57 +0800

    订单支付页布局

--- pages/order_enter/index.js
+++ pages/order_enter/index.js
@@ -5,22 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-    address: wx.getStorageSync("address") || {}
+    address: wx.getStorageSync("address") || {},
+    goods: wx.getStorageSync("goods") || {},
+    // 总价格
+    allPrice: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.handleAllPrice();
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 注意小程序没有computed属性，所以需要封装计算总价格的函数
+  handleAllPrice() {
+    const { goods } = this.data;
+    let price = 0;
 
+    // 开始计算, v就是key，也就是商品id
+    Object.keys(goods).forEach(v => {
+      // 当前商品必须是选中的
+      if (goods[v].selected) {
+        // 单价乘以数量
+        price += (goods[v].goods_price * goods[v].number)
+      }
+    })
+
+    this.setData({
+      allPrice: price
+    })
   },
-
-  
 })
